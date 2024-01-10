09a81c4890adac51c18cfbf964c935e9e424117e
vo1dcod3 <x.fuentealba.ortiz@gmail.com>
2024-01-09 23:00:50 -0300

    pre-entrega1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Carousel from "./components/Carousel"
 
 
 
@@ -7,9 +8,9 @@ const App = () => {
   return (
     <div>
       <Header />
-
+      <Carousel />
       <hr />
-      <Footer/>
+      <Footer />
     </div>
 
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@
 const Footer = () => {
     return (
 
-        <div className="container">
-            <div className="row">
+        <div className="container my-3">
+            <div className="row my-3">
                 <div className="col">
                     <ul className="nav">
                         <li className="nav-item">
@@ -16,7 +16,16 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
+
+            <hr />
+
+            <div className="row" >
+                <div className="col-md-12 text-end">
+                    <p> @ Into The Code</p>
+                </div>
+            </div>
         </div>
+
 
     )
 
